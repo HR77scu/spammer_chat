c6322b30aa3c6b3b4cede157772d29ac688abd5f
azhuraSeishin <azhuraseishin@localhost.localdomain>
2023-07-01 23:18:31 +0700

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 print("progress in 5 seconds ..........")
 time.sleep(10)
 
-for i in range(20):
-    pg.write(' laper di malam hari ');
+for i in range(100):
+    pg.write(' Halo dunia! ');
     time.sleep(0.5)
     pg.press("enter")
