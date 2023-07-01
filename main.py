66553a33c35eac8bc077d50f4b146be925eb257d
azhuraSeishin <azhuraseishin@localhost.localdomain>
2023-07-01 22:18:06 +0700

    fisrt commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui as pg 
+import time
+
+print("progress in 5 seconds ..........")
+time.sleep(10)
+
+for i in range(20):
+    pg.write(' laper di malam hari ');
+    time.sleep(0.5)
+    pg.press("enter")
